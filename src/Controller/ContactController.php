916b41067333723cca2d9bbc3f62ecb0e956716c
AdonaiA <aitgabell@gmail.com>
2022-04-09 02:42:46 +0200

    Add contact form. Add check reservation function for clients.

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Demande;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Security;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(Request $request, EntityManagerInterface $entityManager, TokenStorageInterface $token): Response
+    {
+        $user = $token->getToken()->getUser();
+
+        $demande = new Demande();
+        $form = $this->createForm(ContactType::class, $demande);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $demande->setCreatedAt(new \DateTimeImmutable());
+            $entityManager->persist($demande);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('profile');
+        }
+        return $this->render('contact/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/ContactType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Demande;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('prenom')
+            ->add('nom')
+            ->add('email')
+            ->add('sujet', ChoiceType::class, [
+                'placeholder' => 'Chosissez une sujet',
+                'choices' => $this->getSujetChoices()
+            ])
+            ->add('message')
+        ;
+    }
+
+    public function getSujetChoices(){
+        $choices = [
+            'Je souhaite poser une réclamation',
+            'Je souhaite commander un service supplémentaire',
+            'Je souhaite en savoir plus sur une suite',
+            'J’ai un souci avec cette application'
+        ];
+
+        return array_combine($choices, $choices);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Demande::class,
+        ]);
+    }
+}
